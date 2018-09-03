fdb48561556caf9ce0a29f45fb20a3c600015bf0
User <User@172.30.144.190>
2018-09-03 16:05:14 -0500

    Clase Carrito

--- Procesos/src/modelo/Carrito.java
+++ Procesos/src/modelo/Carrito.java
@@ -27,51 +27,27 @@ public class Carrito {
 	
 		
 		public void agregarJuegoAlPedido(Plato plato){
-			listaPlatos.add(new Plato(plato));
+			listaPlatos.add(plato);
 			
 	    }   
 		
 		public double calcularTotal(){
 			double total=0;                                 
 			for(int i=0; i<listaPlatos.size(); i++){
-				ItemPedido nuevoItem = (ItemPedido) listaPedidos.get(i);
-				total+= nuevoItem.darCantidad() * nuevoItem.darVideoJuego().darPrecioVenta();
+				Plato nuevoPlato = listaPlatos.get(i);
+				total+= cantidadProductos* nuevoPlato.darPrecioVenta();
 				
 			}return total;
 		}
 		
-		/**
-		* Metodo que muestra la lista de todos los videojuegos pedidos por el usuario <br>
-		* <b>pre:</b> El ArrayList listaPedidos esta inicializado <br>
-		* <b>post:</b> Se ha listado todos los videojuegos pedidos <br>
-		* @return lista de los videojuegos pedidos
-		*/
-	    public String listarPedidos(){
-			String lista="";
-			double subtotal=0;
-			
-			for(int i=0; i<listaPedidos.size(); i++){
-				ItemPedido nuevoItem = (ItemPedido) listaPedidos.get(i);
-				subtotal = nuevoItem.darCantidad() * nuevoItem.darVideoJuego().darPrecioVenta();
-				
-				lista += (i+1)+". "+" Nombre:  "+nuevoItem.darVideoJuego().darNombre()+ "  Codigo: "+ nuevoItem.darVideoJuego().darCodigo()+"     \n   Precio: "
-			+ nuevoItem.darVideoJuego().darPrecioVenta()+ "  Unidades:   "+ nuevoItem.darCantidad()+ "  Subtotal:  "+subtotal+"\n";
-			  }return lista;
-		}
 		
-		 /**
-		 * Metodo que elimina un videojuego de la lista videojuegos <br>
-		 * <b>pre:</b> El objeto carroCompra esta inicializado <br>
-		 * <b>post:</b> Se ha eliminado un videojuego del ArrayList <br>
-		 * @param posicionJuego - Posicion que tiene el videojuego en el arreglo. posicionJuego!=null, posicionJuego!="" <br>
-		 */
+	   
 			
-		public void QuitarItemDelPedido(int posicionJuego){
-			listaPedidos.remove(posicionJuego);
+		public void QuitarPlatoDelPedido(int posicionPlato){
+			listaPlatos.remove(posicionPlato);
 		}
 	}
 
 		
 		
 
-}
